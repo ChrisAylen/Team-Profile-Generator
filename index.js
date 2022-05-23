b769578679a060ba3a8b33151e93c15d115da0e6
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-05-23 12:03:01 +0100

    Updated index.js to populate arrays of classes and added an exit menu

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Employee = require("./lib/Employee");
 let menuSlection = '';
+let engineers = [];
+let interns = [];
+let managers = [];
 //All roles are employees
 //We will always want to ask employee questions regardless of role type (name, id, email)
 
@@ -27,7 +30,7 @@ function askEmployeeQuestions() {
         }
     ]).then((response) => {
         console.log(response);
-        employee= new Employee(response.name, response.id, response.email);
+        employee = new Employee(response.name, response.id, response.email);
         return employee;
     });
 }
@@ -63,56 +66,70 @@ function askInternQuestions() {
     ]);
 }
 
+function consloleLogTeam() {
+    console.log(managers);
+    console.log(engineers);
+    console.log(interns);
+}
+
 function displayStartMenu() {
     return inquirer.prompt([
         {
             type: 'list',
             name: 'role',
             message: 'What is the role you wish to add?',
-            choices: ['Engineer', 'Intern', 'Manager', 'Build Team']
+            choices: ['Engineer', 'Intern', 'Manager', 'Build Team', 'Exit']
         }
     ]).then((response) => {
         menuSlection = response.role;
-        askEmployeeQuestions().then((response) => {
-            switch (menuSlection) {
-                case 'Engineer':
-                    askEngineerQuestions().then((response) => {
-                        console.log(response);
-                        const engineer = new Engineer(employee.employeeName, employee.employeeId, employee.employeeEmail, response.github);
-                        console.log(engineer);
-                        return engineer;
-                    });
-                    break;
-                case 'Intern':
-                    askInternQuestions().then((response) => {
-                        console.log(response);
-                        const intern = new Intern(employee.employeeName, employee.employeeId, employee.employeeEmail, response.school);
-                        var test = intern.getEmployeeRole();
-                        console.log(test);
-                        console.log(intern);
-                        return intern;
-                    });
-                    break;
-                case 'Manager':
-                    askManagerQuestions().then((response) => {
-                        console.log(response);
-                        const manager = new Manager(employee.employeeName, employee.employeeId, employee.employeeEmail, response.officeNumber);
-                        console.log(manager)
-                        return manager;
-                    });
-                    break;
-                case 'Build Team':
-                    promptUser().then((response) => {
-                        console.log(response);
-                    });
-                    break;
-            }
+        if (menuSlection != 'Exit') {
 
+            askEmployeeQuestions().then((response) => {
+                switch (menuSlection) {
+                    case 'Engineer':
+                        askEngineerQuestions().then((response) => {
+                            console.log(response);
+                            const engineer = new Engineer(employee.employeeName, employee.employeeId, employee.employeeEmail, response.github);
+                            //console.log(engineer);
+                            engineers.push(engineer);
+                            consloleLogTeam();
+                            displayStartMenu()
+                            return engineer;
+                        });
+                        break;
+                    case 'Intern':
+                        askInternQuestions().then((response) => {
+                            console.log(response);
+                            const intern = new Intern(employee.employeeName, employee.employeeId, employee.employeeEmail, response.school);
+                            interns.push(intern);
+                            //var test = intern.getEmployeeRole();
+                            consloleLogTeam();
+                            displayStartMenu()
+                            return intern;
+                        });
+                        break;
+                    case 'Manager':
+                        askManagerQuestions().then((response) => {
+                            console.log(response);
+                            const manager = new Manager(employee.employeeName, employee.employeeId, employee.employeeEmail, response.officeNumber);
+                            managers.push(manager);
+                            consloleLogTeam();
+                            displayStartMenu()
+                            return manager;
+                        });
+                        break;
+                    case 'Build Team':
+                        promptUser().then((response) => {
+                            console.log(response);
+                        });
+                        break;
+                }
+
+            });
         }
-
-
-
-        );
+        else {
+            process.exit();
+        }
     });
 
 }
